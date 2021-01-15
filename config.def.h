7335ad4af9d326eb6740be157aed887bb8588768
Noah Geller <noah.stanford.geller@gmail.com>
2021-01-14 23:13:57 -0800

    config

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "emoji:size=12" };
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#888888";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
