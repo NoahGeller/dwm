326c886c159f16339048b0ddefd951e3e7e94e02
Noah Geller <noah.stanford.geller@gmail.com>
2021-01-14 19:16:38 -0800

    fixed

--- config.def.h
+++ config.def.h
@@ -8,9 +8,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "emoji:size=12" };
 static const int user_bh            = 20;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
->>>>>>> barheight
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
