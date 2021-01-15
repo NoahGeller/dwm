4de74dae7001b248d06292e40ea9b3561b0bb5d4
Noah Geller <noah.stanford.geller@gmail.com>
2021-01-14 23:15:02 -0800

    config

--- config.def.h
+++ config.def.h
@@ -13,8 +13,8 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray2, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1, col_gray1 },
+	[SchemeNorm] = { col_gray2, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_gray1, col_gray2 },
 };
 
 /* tagging */
